fa176e66c1b04e138c99421343cc47e2347f01bd
Markus Ullmann <jokey@gentoo.org>
2006-06-15 23:35:18 +0000

    move everything to sunrise subdirectory
    svn path=/; revision=70

---
+++ scripts/create-stats.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+bugsin=$(ls */*/ChangeLog -d | xargs grep -i bug | grep -oE "[0-9]{2}[0-9]+")
+ebuildsin=$(ls */*/ChangeLog -d | xargs grep -il bug | sed -e "s:/ChangeLog::")
+
+declare -a bugs ebuilds cc
+
+a=0
+for i in ${ebuildsin}; do
+	ebuilds[$a]=${i}
+	a=$[ $a+1 ];
+done
+
+a=0
+for i in ${bugsin}; do
+	bugs[$a]=${i}
+	a=$[ $a+1 ];
+done
+
+for ((i=0;i<${#ebuilds[@]};i++)) do
+	cc[$i]=$(wget bugs.gentoo.org/${bugs[$i]} -q -O - | sed -e "s:&#64;:@:" | grep "value=.*@gentoo.org" | sed -e 's:[^"]*"\([^@]*\).*:\1:' | sed "s:maintainer-wanted::");
+	if [ -z "$1" ] || [ "${cc[$i]/$1}" != "${cc[$i]}" ]; then
+		echo ${ebuilds[$i]} - bug ${bugs[$i]} - on CC: ${cc[$i]};
+	fi
+done
