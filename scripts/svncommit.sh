361cb998a33c4c20c31e61ec8a0a3d538681d974
Stefan Schweizer <genstef@gentoo.org>
2006-06-25 22:29:22 +0000

    svncommit.sh - generate the digests, add to svn and commit
    svn path=/sunrise/; revision=310

---
+++ scripts/svncommit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "error: you need to supply a commit message"
+	exit 1
+fi
+if ! ls *.ebuild >/dev/null; then
+	echo "error: you need to be in an ebuild directory"
+	exit 1
+fi
+
+for i in *.ebuild; do
+ebuild $i digest
+done
+svn add *.ebuild Manifest files/digest-*
+svn ci -m $1
