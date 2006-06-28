4e1c2e4f360b7324b87a0ffe7e260717ad6c251c
Stefan Schweizer <genstef@gentoo.org>
2006-06-28 23:24:08 +0000

    net-firewall/nufw - initd,confd: remove gentoo headers
    svn path=/sunrise/; revision=360

--- net-firewall/nufw/files/nuauth-conf.d
+++ net-firewall/nufw/files/nuauth-conf.d
@@ -1,5 +1,2 @@
-# Copyright 1999-2006 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: $
 
 NUAUTH_OPTIONS=""
--- net-firewall/nufw/files/nufw-conf.d
+++ net-firewall/nufw/files/nufw-conf.d
@@ -1,5 +1,2 @@
-# Copyright 1999-2006 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: $
 
 NUFW_OPTIONS="-k /etc/nufw/nufw-key.pem -c /etc/nufw/nufw-cert.pem -d 127.0.0.1 -p 4129"
