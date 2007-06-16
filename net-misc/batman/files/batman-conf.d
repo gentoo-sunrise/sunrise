9b4d403ba68ee4a9d1c374e707bf057fd8d46352
Cédric Krier <cedk@gentoo.org>
2007-06-16 23:32:16 +0000

    net-misc/batman: In portage now
    svn path=/sunrise/; revision=3953

--- net-misc/batman/files/batman-conf.d
+++
@@ -1,9 +0,0 @@
-#/etc/conf.d/batman: config file for /etc/init.d/batman
-
-# Configure wich interface or interfaces to for batman to use.
-# list all interfaces space separated.
-BATMAN_INTERFACES="eth0"
-
-# Insert any other batman options
-BATMAN_OPTIONS="-d 0"
-
