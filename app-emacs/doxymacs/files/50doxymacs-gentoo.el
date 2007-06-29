2ab845fa32e351e4c8857774b17bdad7256cab01
Christian Faulhammer <opfer@gentoo.org>
2007-06-29 07:56:19 +0000

    Improved ebuild in Emacs overlay now, so no need to duplicate it
    svn path=/sunrise/; revision=4078

--- app-emacs/doxymacs/files/50doxymacs-gentoo.el
+++
@@ -1,9 +0,0 @@
-
-;;; doxymacs site-lisp configuration
-(require 'doxymacs)
-(add-hook 'c-mode-common-hook 'doxymacs-mode)
-(defun my-doxymacs-font-lock-hook ()
-  (if (or (eq major-mode 'c-mode) (eq major-mode 'c++-mode))
-      (doxymacs-font-lock)))
-(add-hook 'font-lock-mode-hook 'my-doxymacs-font-lock-hook)
-
