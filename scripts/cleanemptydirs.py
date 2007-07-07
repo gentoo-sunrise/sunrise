ad2a1716f80403428eefdc32403a7db227705556
Markus Ullmann <jokey@gentoo.org>
2007-07-07 17:54:00 +0000

    add nice lil script to find empty dirs in sunrise
    svn path=/sunrise/; revision=4189

---
+++ scripts/cleanemptydirs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""find empty dirs"""
+
+import os
+from os.path import join, getsize
+for root, dirs, files in os.walk('.'):
+    for ignoredir in ('CVS','.svn','xfce-base','xfce-extra','eclass'):
+        if ignoredir in dirs:
+            dirs.remove(ignoredir)
+    if len(dirs)+len(files) == 0:
+        print root, "seems to be empty"
