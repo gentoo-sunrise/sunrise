e938794a640263ccdf759904ca5a05e28c5ba4cf
John (EBo) David <ebo@users.sourceforge.net>
2009-08-15 21:34:52 +0000

    sys-cluster/globus: updated globus init.d script
    svn path=/sunrise/; revision=9018

--- sys-cluster/globus/files/globus-init.d
+++ sys-cluster/globus/files/globus-init.d
@@ -3,28 +3,64 @@
 # Distributed under the terms of the GNU General Public License v2
 # $Header: $
 
-source ${GLOBUS_LOCATION}/etc/globus-user-env.sh
+GLOBUS_LOCATION=/opt/globus4
+
+source "${GLOBUS_LOCATION}"/etc/globus-user-env.sh
 
 depend() {
-    need net
+	after localmount netmount nfsmount dns
+	need net
 }
 
+PIDFILE="/var/run/globus.pid"
+
+checkconfig() {
+# FIXME: find an appropriate file -- like the certificates or something
+#	if [ ! -e /etc/SOME_FILE... ] ; then
+#		eerror "You need an SOME_FILE file to run globus"
+#		return 1
+#	fi
+
+	# verify presence of server binary
+	if ! [ -x "${GLOBUS_LOCATION}/bin/globus-start-container" ]; then
+		eerror "Could not find executable ${GLOBUS_LOCATION}/bin/globus-start-container"
+		return 1
+	fi
+}
+
+
 start() {
-    ebegin "Starting Globus WS Container"
-    ${GLOBUS_LOCATION}/bin/globus-start-container ${CONTAINER_OPTS} >
-${LOG_FILE} 2>&1 &
-    eend ${?}
+	checkconfig || return 1
+
+	ebegin "Starting Globus WS Container"
+	start-stop-daemon --start --quiet --pidfile "${PIDFILE}" \
+		--exec "${GLOBUS_LOCATION}"/bin/globus-start-container -- \
+			${CONTAINER_OPTS}
+
+#		--stdout "${LOG_FILE}" \
+
+	eend $?
 }
 
 stop() {
-    ebegin "Stopping Globus WS Container"
-        ${GLOBUS_LOCATION}/bin/grid-proxy-init \
-                        -cert /etc/grid-security/containercert.pem \
-                        -key /etc/grid-security/containerkey.pem \
-                        -out /tmp/containerproxy.pem  > /dev/null
-        export X509_USER_PROXY=/tmp/containerproxy.pem
-        ${GLOBUS_LOCATION}/bin/globus-stop-container
-        export  X509_USER_PROXY
-        rm /tmp/containerproxy.pem
-    eend ${?}
+	ebegin "Stopping Globus WS Container"
+
+	local container ret
+	container="$(mktemp -d)"
+
+	start-stop-daemon --stop --quiet --pidfile "${PIDFILE}" \
+		--stdout "${LOG_FILE}" \
+		--exec "${GLOBUS_LOCATION}"/bin/grid-proxy-init -- \
+						-cert /etc/grid-security/containercert.pem \
+						-key  /etc/grid-security/containerkey.pem \
+						-out  "${container}"/containerproxy.pem 
+
+	X509_USER_PROXY=${container}/containerproxy.pem \
+		"${GLOBUS_LOCATION}"/bin/globus-stop-container
+
+	ret = $? # cache globus-stop-container's return code.
+
+	rm -rf "${container}"
+    
+	eend ${ret}
 }
