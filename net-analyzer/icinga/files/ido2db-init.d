7f7005c1d19f8a1531acc829e97d247373b1b76c
Matthew Thode (prometheanfire) <mthode@mthode.org>
2011-05-09 18:32:40 +0000

    net-analyzer/icinga: change to ido2db init, thanks to Wolfgang Schricker
    svn path=/sunrise/; revision=12050

--- net-analyzer/icinga/files/ido2db-init.d
+++ net-analyzer/icinga/files/ido2db-init.d
@@ -4,6 +4,16 @@
 # $Header: $
 
 IDO2DBBIN="/usr/sbin/ido2db"
+SOCKET="/var/icinga/ido.sock"
+
+function check() {
+	if [[ -S "${SOCKET}" ]] ; then
+		ewarn "Strange, the socket file already exist in \"${SOCKET}\""
+		ewarn "it will be removed now and re-created by ido2db"
+		ewarn "BUT please make your checks."
+		rm -f "${SOCKET}"
+	fi
+}
 
 depend() {
 	need net
@@ -12,6 +22,7 @@ depend() {
 }
 
 start() {
+	check
 	ebegin "Starting ido2db"
 	start-stop-daemon --quiet --start --pidfile /var/icinga/ido2db.lock --startas ${IDO2DBBIN} -- -c ${IDO2DBCFG}
 	eend $?
